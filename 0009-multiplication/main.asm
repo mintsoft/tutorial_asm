c1b9ecdbf6a54dd135d56074a42475bb17f16b08
Rob <rob@mintsoft.net>
2017-12-26 22:44:48 +0000

    This multiples '3' by '2' and outputs the result, things to note here is that the "mul" instruction, multiples al by whatever register is passed in (presumably)

---
+++ 0009-multiplication/main.asm
@@ -0,0 +1,36 @@
+section .text
+	global _start
+_start:
+	mov al, '3'
+	sub al,'0'
+
+	mov bl,'2'
+	sub bl, '0'
+
+	mul bl	;this multiplies the value of al by the value in bl (in-place)
+
+	add al,'0'	;convert back to binary
+
+	mov [result], al 	;store result for later
+
+	mov ecx,msg
+	mov ebx,len
+	mov ebx, 1 ;stdout
+	mov eax,4 ; print
+	int 80h;
+
+	mov ecx,result
+	mov edx,1
+	mov ebx,1 ;stdout
+	mov eax,4 ;print	
+	int 80h
+
+	mov eax,1
+	mov ebx,0
+	int 80h
+
+section .data
+msg db "result result is: ",0xA
+len equ $-msg
+segment .bss
+result resb 1
