8253beba38d8914b26bcc9a0f409378b36068d62
Rob <rob@mintsoft.net>
2017-12-26 22:53:35 +0000

    This divides 8 by 2; (the value of ax is the numerator and the value of whatever register is passed to "div" is the denominator); this is integer division; it seems to work if you convert ax to eax too

---
+++ 0010-division/main.asm
@@ -0,0 +1,35 @@
+section .text
+	global _start
+_start:
+	mov ax, 8
+	mov bl, 2
+
+	div bl	;divides ax by bl in-place
+	add ax, '0'	;convert back to ascii!
+
+	mov [result], ax
+
+;print message
+	mov ecx,msg
+	mov edx,len
+	mov ebx,1 ;stdout
+	mov eax,4; write
+	int 80h
+
+;print result!
+	mov ecx,result
+	mov edx,1
+	mov ebx,1;stdout
+	mov eax,4
+	int 80h
+
+	mov eax,1
+	mov ebx,0
+	int 80h
+
+section .data
+msg db "result result is: ",0xA
+len equ $-msg
+
+segment .bss
+result resb 1
