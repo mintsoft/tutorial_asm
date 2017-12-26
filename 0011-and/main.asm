356d198be5d228712c8ea541dd1cb732165c263b
Rob <rob@mintsoft.net>
2017-12-26 23:06:12 +0000

    Does a binary AND on a register and JZ (jump to label if value of ax is 0 - I assume) to perform branching

---
+++ 0011-and/main.asm
@@ -0,0 +1,36 @@
+section .text
+   global _start            ;must be declared for using gcc
+	
+_start:           
+	mov ax,8
+	and ax,1	;BAND ax with 1 (this will result in 0)
+	jz  evnn	; jump to label evnn IF ax is 0
+	
+	mov eax,4	;sys_write
+	mov ebx,1	;stdout
+	mov ecx,odd_msg	;message
+	mov edx,odd_msg_len	;length
+	int 80h;
+	
+	jmp outprog
+
+evnn:
+	mov ah,9		;not sure why this is here
+	
+	mov eax,4	;sys_write
+	mov ebx,1	;stdout
+	mov ecx,even_msg
+	mov edx,even_msg_len
+	int 80h
+
+outprog:
+	mov eax,1
+	mov ebx,0
+	int 80h
+
+section .data
+even_msg db "Even Number!"
+even_msg_len equ $-even_msg
+
+odd_msg db "Odd Number!"
+odd_msg_len equ $-odd_msg
