8279381a3495cf4e3bc5b5f3b9b590b3b2ecb481
Rob <rob@mintsoft.net>
2017-12-27 12:15:12 +0000

    so adc is add with carry, this adds the digits together but sets the carry flag; we need to preserve this for the next adc operation which is why we pushf and popf as the or operation will clear the flags!

--- 0016-bcd-ascii/main.asm
+++ 0016-bcd-ascii/main.asm
@@ -9,13 +9,30 @@ clc		;compare logical characters (whatever that means)
 add_loop:
 	mov al, [num1+esi]	; copies into al, the value of the nth character (esi being n) of num1
 	adc al, [num2+esi]	; adds onto al the value of the nth character (esi being n) of num2
+				; adc means add with carry
 	aaa			; ASCII Adjust After Addition - operates on al
-	pushf			; we want the value of something for later
+	pushf			; push flags as we will need to preserve the carry flag for the next loop
 	or al, 30h		; convert al to ascii?
-	popf			; we want our saved value back
+	popf			; pop flags  back off as we need it for the next loop
 
 	mov [sum+esi],al	; using our sum buffer copy the nth digit from al into the buffer
 	dec esi			; decrement our string index (move along one char)
+
+push ecx
+;lets output the sum on every loop
+mov edx,5
+mov ecx,sum
+mov ebx,1
+mov eax,4
+int 80h
+
+mov edx,1
+mov ecx,nl
+mov ebx,1
+mov eax,4
+int 80h
+pop ecx
+
 	loop add_loop		; go back to the start with a reduced ecx
 
 mov edx,len
@@ -38,6 +55,7 @@ int 80h
 
 section .data
 msg db 'The sum is: ',0xA
+nl db 0xA
 len equ $- msg
 num1 db '12345'
 num2 db '23456'
