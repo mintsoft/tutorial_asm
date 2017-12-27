f5dcd4dd1e6090611067213d66571828f8eb4f27
Rob <rob@mintsoft.net>
2017-12-27 12:51:38 +0000

    Outputs ASCII characters

---
+++ 0019-stack/main.asm
@@ -0,0 +1,30 @@
+section	.text
+   global _start        ;must be declared for using gcc
+	
+_start:	                ;tell linker entry point
+	call    display
+	mov eax,1
+	int 80h
+
+display:
+	mov ecx,256
+
+next:
+	push ecx	; we need ecx for sys_write so store it!
+
+	mov eax,4	;sys_write
+	mov ebx,1	;stdout
+	mov ecx,achar
+	mov edx,1
+	int 80h
+
+	pop ecx		; get our ecx back
+
+	mov dx, [achar]
+	cmp byte [achar], 0dh
+	inc byte [achar]
+	loop next
+ret
+
+section .data
+achar db '0'
