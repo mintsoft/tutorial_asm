530348b47fdb7554deb474b301afc6445d2aabc1
Rob <rob@mintsoft.net>
2017-12-27 14:00:34 +0000

    so this allocated memory but also 0's it out. It 0's it out using the rep (unconditional repeat) operation with the STOSD (stores the contents of eax into edi) I don't understand why we mov edi, eax. that seems to be overwriting it with 0?

--- 0023-malloc/main.asm
+++ 0023-malloc/main.asm
@@ -16,15 +16,17 @@ cmp eax,0	;sets eax to -1 if there was an erro
 jl	exit	; if less than 
 
 mov edi, eax	;edi = highest available address
+
+; this gives us our starting point for the below loop
 sub edi,4	;location of the highest DWORD (4bytes)
-mov ecx,4096	; number of DWORDS allocated (16384/4)
-xor eax,eax	;reset eax to 0
+mov ecx,4096	; number of DWORDS allocated (16384/4)	; this is the implicit iterator of rep
+xor eax,eax	;reset eax to 0	- this is the value that stosd will write into edi
 
-; I don't know how this section works::
-std	; ---- don't know
-rep stosd	;; repeat for entire allocated area??
-cld		;put df flag to normal state
-
+std		; this sets the direction flag - opposite of CLD (i.e. makes the operation right to left)
+rep stosd	; this repeats until cx is 0; which we set above to be 4096; so this is resetting the 16k we allocated to be
+		; 0 in blocks of 4 bytes at a time (presumably at 32 bit, the most efficient way of doing it)
+		; but REP actually runs the operation you give it, so we're running STOSD here, which copies EAX to ES:DI (edi for short)
+cld		; reset the df (direction flag) to normal state (left to right)
 
 mov eax,4
 mov ebx,1
